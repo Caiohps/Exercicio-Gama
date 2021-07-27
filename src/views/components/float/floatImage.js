7a9432a3b0955251b1bead0751d4796b963153c5
Caiohps <caiohpsilva@gmail.com>
2021-07-27 11:47:15 -0300

    First commit

---
+++ src/views/components/float/floatImage.js
@@ -0,0 +1,9 @@
+import YodaBaby from '../../../assets/images/elements/baby-yoda.png';
+
+let floatImage = `
+<div class="main-left">
+    <a href="#darthvader"><img src="${YodaBaby}" alt="Yoda Baby" width="120px" height="auto"></a>
+</div>
+`
+
+export default floatImage;
---
+++ src/views/components/footer/index.js
@@ -0,0 +1,12 @@
+import Planet from '../../../assets/images/elements/planet.png';
+
+let footer = `
+<div class="container footer-contain">
+    <p class="footer-paragrafo">
+     Star wars - este é um projeto para </br>Módulo de boas práticas.
+    </p>
+    <a href="#top"><img class="planet" src="${Planet}" alt="planet"></a>
+</div>
+`
+
+export default footer;
